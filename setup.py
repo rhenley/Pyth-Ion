49198c4542f3735dd9726b893ba02af4a259dd0f
Robert Henley <roberthenley89@gmail.com>
2016-08-30 08:44:13 -0400

    test

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
 
 APP = ['Pythion']
 
@@ -7,7 +10,8 @@ setup(name='Pythion',
       description='Nanopore Data Analysis Package',
       author='Robert Henley',
       author_email='roberthenley89@gmail.com',
-      url='https://github.com/rhenley/Pyth-Ion/',
+      url='https://github.com/rhenley/Pyth-Ion/', 
+      packages=find_packages()
 #      data_files=[('C:\Users\rober\Documents\GitHub\Pyth-Ion', 
 #                   ['3500bp-200mV.mat','3500bp-200mV.log'])]
      )
