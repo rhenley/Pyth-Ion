44c6c18190e95865933e720c17aaf6f195cbc59b
Robert Henley <roberthenley89@gmail.com>
2016-08-30 08:37:33 -0400

    test

--- setup.py
+++ setup.py
@@ -8,7 +8,5 @@ setup(name='Pythion',
       author='Robert Henley',
       author_email='roberthenley89@gmail.com',
       url='https://github.com/rhenley/Pyth-Ion/',
-      packages = ['Pythion'],
-#      scripts = ['main.py','PlotGUI.py'],
       data_files=[('Sample Data', ['3500bp-200mV.mat','3500bp-200mV.log'])]
      )
