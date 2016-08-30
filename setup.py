857a0d8f70f10df1852a3e0f04a5a45f2dc174f5
Robert Henley <roberthenley89@gmail.com>
2016-08-30 08:40:32 -0400

    test

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(name='Pythion',
       author='Robert Henley',
       author_email='roberthenley89@gmail.com',
       url='https://github.com/rhenley/Pyth-Ion/',
-      data_files=[('C:\Users\rober\Documents\GitHub\Pyth-Ion', 
-                   ['3500bp-200mV.mat','3500bp-200mV.log'])]
+#      data_files=[('C:\Users\rober\Documents\GitHub\Pyth-Ion', 
+#                   ['3500bp-200mV.mat','3500bp-200mV.log'])]
      )
