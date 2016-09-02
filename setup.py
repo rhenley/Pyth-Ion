b4d923b28f75d42ef262034d85f83cd094410c6b
Robert Henley <roberthenley89@gmail.com>
2016-09-02 15:51:05 +0200

    test

--- setup.py
+++ setup.py
@@ -9,4 +9,5 @@ setup(
     author='Robert Henley',
     author_email='roberthenley89@gmail.com',
     package_dir=['PythIon'],
-    description='Nanopore Data Analysis package.')
+    description='Nanopore Data Analysis package.',
+    )
